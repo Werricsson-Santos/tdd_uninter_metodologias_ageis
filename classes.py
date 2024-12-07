8f2baaa5bf5b0c39df6aecc54b0bf0da81883756
Werricsson <werricsson.santos@hotmail.com>
2024-12-07 16:08:18 -0300

    Implementação da classe Produto (Green step)

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Produto:
+    def __init__(self, idProduto, marca, tipo, quantidade, valor):
+        self.idProduto = idProduto
+        self.marca = marca
+        self.tipo = tipo
+        self.quantidade = quantidade
+        self.valor = valor
+    
+class Funcionario:
+    #implementação da classe funcionario
+    print("WiP")
